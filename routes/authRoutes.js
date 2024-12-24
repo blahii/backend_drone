b6223a4379309246b54ea39ce490ac716d6f63fd
blahii <blahiidima@gmail.com>
2024-12-24 14:04:47 +0100

    Initial commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
 // Ensure the path is correct
-const { register, login } = require('../controllers       /authController'); // Check this path
+const { register, login } = require('../controllers/authController'); // Check this path
 
 const router = express.Router();
 
-router.post('/register', register); // Обработчик регистрации
-router.post('/login', login);       // Обработчик логина
+router.post('/register', register); // Handler for registration
+router.post('/login', login);       // Handler for login
+
+console.log('Register function:', register);
+console.log('Login function:', login);
 
 module.exports = router;
